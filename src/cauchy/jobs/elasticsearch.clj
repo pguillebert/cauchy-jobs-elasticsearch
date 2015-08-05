92c47c5dcfd9293767f4ddd9bc39d372374e9217
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2015-08-05 12:09:29 +0200

    [FEATURE] add elasticsearch-node-stats jobs

--- src/cauchy/jobs/elasticsearch.clj
+++ src/cauchy/jobs/elasticsearch.clj
@@ -56,23 +56,54 @@
                  initializing_shards active_primary_shards]
           :as health} (fetch-health conf)
          stats (fetch-stats conf)]
-     [{:service "color" :state (color->state status)}
-
+     [
+      ;;Cluster metrics
+      {:service "color" :state (color->state status)}
       {:service "local_active_shards"
        :metric (count-local-active-shards conf)}
-
       {:service "docs_in_cluster"
        :metric (get-in stats [:_all :primaries :docs :count])}
-
       {:service "fielddata_memory_size_in_bytes"
        :metric (get-in stats [:_all :total :fielddata :memory_size_in_bytes])}
-
       {:service "fielddata_evictions"
        :metric (get-in stats [:_all :total :fielddata :evictions])}
-
       {:service "active_shards" :metric active_shards}
       {:service "unassigned_shards" :metric unassigned_shards}
       {:service "relocating_shards" :metric relocating_shards}
       {:service "initializing_shards" :metric initializing_shards}
-      {:service "active_primary_shards" :metric active_primary_shards}]))
+      {:service "active_primary_shards" :metric active_primary_shards}
+      ]))
   ([] (elasticsearch-health {})))
+
+(defn elasticsearch-node-stats
+    ([{:keys [host port] :as conf}]
+      (let [ [node-id node-stats] (first (get-in (fetch-local conf) [:nodes]))]
+        (vec (concat [
+        ;;Node indices metrics
+        {:service "indices.indexing.total" :metric (get-in node-stats [:indices :indexing :index_total])}
+        {:service "indices.indexing.current" :metric (get-in node-stats [:indices :indexing :index_current])}
+        {:service "indices.get.total" :metric (get-in node-stats [:indices :get :total])}
+        {:service "indices.get.current" :metric (get-in node-stats [:indices :get :current])}
+        {:service "indices.search.query_total" :metric (get-in node-stats [:indices :search :query_total])}
+        {:service "indices.search.query_current" :metric (get-in node-stats [:indices :search :query_current])}
+        {:service "indices.search.open_contexts" :metric (get-in node-stats [:indices :search :open_contexts])}
+        ;;Node jvm metrics
+        {:service "gc.young.count" :metric (get-in node-stats [:jvm :gc :collectors :young :collection_count])}
+        {:service "gc.young.collection_time_in_millis" :metric (get-in node-stats [:jvm :gc :collectors :young :collection_time_in_millis])}
+        {:service "gc.old.count" :metric (get-in node-stats [:jvm :gc :collectors :old :collection_count])}
+        {:service "gc.old.collection_time_in_millis" :metric (get-in node-stats [:jvm :gc :collectors :old :collection_time_in_millis])}
+        ]
+        ;;Thread data
+        (mapcat
+          (fn [[thread_type thread_data]]
+            [{ :service (str "thread_pool." (name thread_type) ".nb") :metric (get-in thread_data [:threads]) }
+            { :service (str "thread_pool." (name thread_type) ".queue") :metric (get-in thread_data [:queue]) }
+            { :service (str "thread_pool." (name thread_type) ".completed") :metric (get-in thread_data [:completed]) }
+            { :service (str "thread_pool." (name thread_type) ".largest") :metric (get-in thread_data [:largest]) }
+            ]
+          ) (get-in node-stats [:thread_pool])
+        )
+        ))
+      ))
+  ([] (elasticsearch-node-stats {}))
+)
